ecfa02be0bc48aa772236a2f0d271c32ce73a008
Mohammed Atallah <mohamedatallahh2017@gmail.com>
2022-11-01 06:34:56 +0200

    Initial setup

---
+++ src/pages/index.jsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Amazon</title>
+      </Head>
+
+      <section>
+        <h1>Amazon!</h1>
+      </section>
+    </>
+  );
+}
